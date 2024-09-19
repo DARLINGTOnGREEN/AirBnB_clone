697ddbef2f63de9e1db46c62e797c013ea7ebd1e
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-09-19 16:58:12 +0100

    upload

---
+++ console.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+"""Module that defines HBNBCommand class"""
+
+
+import cmd
+from models.base_model import BaseModel
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """this is the console class"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, _):
+        """ to exit console"""
+        return True
+
+    def do_EOF(self, _):
+        """ctrl+D to quit"""
+        return True
+
+    def emptyline(self):
+        """creates emptylines"""
+        pass
+
+    def do_create(self, line):
+        """Creates a new instance of basemodel"""
+        args = line.split(" ")
+        print(args, len(args))
+        if line == "":
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            obj = BaseModel()
+            print(obj.id)
+            storage.save()
+
+    def do_show(self, line):
+        """Prints the string representation of an
+
+        instance based on the class name and id"""
+        args = line.split(" ")
+        if line == "":
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) != 2:
+            print("** instance id missing **")
+        else:
+            o = None
+            for _, n in storage.all().items():
+                if n.id == args[1]:
+                    o = n
+                    break
+
+            print(o) if o is not None else print("** no instance found **")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        args = line.split(" ")
+        if line == "":
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) != 2:
+            print("** instance id missing **")
+        else:
+            o = None
+            for _, n in storage.all().items():
+                if n.id == args[1]:
+                    o = n
+                    break
+
+            if o is not None:
+                del storage.all()[f"{o.__class__.__name__}.{o.id}"]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, line):
+        """Prints all string representation of all instances"""
+        args = line.split(" ")
+
+        if line != "" and line == "BaseModel":
+            # print the basemode object
+            all = storage.all()
+            all_list = []
+            for m, n in all.items():
+                if type(n) is BaseModel:
+                    all_list.append(n)
+
+            print("[", end="")
+            for idx in range(len(all_list)):
+                print(all_list[idx], end="")
+                if idx + 1 == len(all_list):
+                    print("]")
+                else:
+                    print(", ", end="")
+
+        elif line == "":
+            # print all objects
+            pass
+        elif line != "" and line != "BaseModel":
+            # class doesn't exist
+            pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
